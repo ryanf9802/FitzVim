6f69d4ad8005bd0264beabcf4b1882462ba0ff0b
Ryan Fitzpatrick <ryanf9802@gmail.com>
2025-04-15 21:30:21 -0400

    fixed commands to Aienable and Aidisable

--- lua/core/commands.lua
+++ lua/core/commands.lua
@@ -1,9 +1,9 @@
-vim.api.nvim_create_user_command("aidisable", function()
+vim.api.nvim_create_user_command("Aidisable", function()
   require("supermaven-nvim.api").stop()
   print("AI Completion disabled for current buffer")
 end, {})
 
-vim.api.nvim_create_user_command("aienable", function()
+vim.api.nvim_create_user_command("Aienable", function()
   require("supermaven-nvim.api").start()
   print("AI Completion enabled for current buffer")
 end, {})
