8dc9516c7426fc97f138d3204c04ee05d35955c0
Ryan Fitzpatrick <ryanf9802@gmail.com>
2025-04-17 00:38:23 -0400

    removed supermaven from cmp sources so only inline completions happen

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -27,7 +27,7 @@ return {
       sources = cmp.config.sources({
         { name = "nvim_lsp" },
         { name = "luasnip" },
-        { name = "supermaven" },
+--        { name = "supermaven" },
       }, {
         { name = "buffer" },
       }),
