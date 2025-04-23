7cf66609a45580bc201a18297ce5f0282ae55e00
Ryan Fitzpatrick <ryanf9802@gmail.com>
2025-04-23 14:07:07 -0400

    lualine and tabbar customized... still having occasional, inconsistent theme issue

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,47 +1,47 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
 
-      local python_utils = require("utils.python")
-      local venv_python = python_utils.find_venv_python()
+			local python_utils = require("utils.python")
+			local venv_python = python_utils.find_venv_python()
 
-      lspconfig.pyright.setup({
-        settings = {
-          python = {
-            pythonPath = venv_python or "python",
-          },
-        },
-      })
+			lspconfig.pyright.setup({
+				settings = {
+					python = {
+						pythonPath = venv_python or "python",
+					},
+				},
+			})
 
-      vim.diagnostic.config({
-        virtual_text = true,
-        signs = true,
-        underline = true,
-        update_in_insert = false,
-        severity_sort = true,
-      })
-    end,
-  },
-  {
-    "williamboman/mason.nvim",
-    build = ":MasonUpdate",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = {
-      "williamboman/mason.nvim",
-      "neovim/nvim-lspconfig",
-    },
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "pyright", "ts_ls", "lua_ls" },
-        automatic_installation = true,
-      })
-    end,
-  },
+			vim.diagnostic.config({
+				virtual_text = true,
+				signs = true,
+				underline = true,
+				update_in_insert = false,
+				severity_sort = true,
+			})
+		end,
+	},
+	{
+		"williamboman/mason.nvim",
+		build = ":MasonUpdate",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = {
+			"williamboman/mason.nvim",
+			"neovim/nvim-lspconfig",
+		},
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "pyright", "ts_ls", "lua_ls" },
+				automatic_installation = true,
+			})
+		end,
+	},
 }
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,44 +1,96 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  -- event = { "BufReadPost", "BufNewFile" },
-  config = function()
-    vim.opt.laststatus = 3
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		vim.opt.laststatus = 3
 
-    require("lualine").setup {
-      options = {
-        icons_enabled     = true,
-        theme             = "auto",
-        component_separators = { left = "", right = "" },
-        section_separators   = { left = "", right = "" },
-        disabled_filetypes   = { "lazy", "NvimTree", "dashboard" },
-        always_show_tabline = true,
-        globalstatus = true,
-        refresh = {
-          statusline = 100,
-          tabline = 100,
-          winbar = 100,
-        },
-      },
-      sections = {
-        lualine_a = { "mode" },
-        lualine_b = { "branch", "diff" },
-        lualine_c = { "filename" , {
-          "diagnostics",
-          sources = { "nvim_lsp" },
-        }},
-        lualine_x = { "encoding", "fileformat", "filetype" },
-        lualine_y = { "progress" },
-        lualine_z = { "location" },
-      },
-      inactive_sections = {
-        lualine_a = {},
-        lualine_b = {},
-        lualine_c = { "filename" },
-        lualine_x = { "location" },
-        lualine_y = {},
-        lualine_z = {},
-      },
-    }
-  end,
+		require("lualine").setup({
+			options = {
+				icons_enabled = true,
+				theme = "auto",
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
+				disabled_filetypes = { "lazy", "NvimTree", "dashboard" },
+				always_show_tabline = true,
+				globalstatus = true,
+				refresh = {
+					statusline = 100,
+					tabline = 100,
+					winbar = 100,
+				},
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff" },
+				lualine_c = { "filename", {
+					"diagnostics",
+					sources = { "nvim_lsp" },
+				} },
+				lualine_x = {
+					"encoding",
+					"filetype",
+					{
+						"fileformat",
+						symbols = {
+							unix = "", -- default ''
+							dos = "", -- default ''
+							mac = "", -- default ''
+						},
+					},
+				},
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+			inactive_sections = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = { "filename" },
+				lualine_x = { "location" },
+				lualine_y = {},
+				lualine_z = {},
+			},
+			tabline = {
+				lualine_a = {},
+				lualine_b = {
+					{
+						"tabs",
+						max_length = vim.o.columns / 2,
+
+						mode = 0, -- 0: Shows tab_nr
+						-- 1: Shows tab_name
+						-- 2: Shows tab_nr + tab_name
+
+						path = 0, -- 0: just shows the filename
+						-- 1: shows the relative path and shorten $HOME to ~
+						-- 2: shows the full path
+						-- 3: shows the full path and shorten $HOME to ~
+					},
+				},
+				lualine_c = { "windows" },
+				lualine_x = { {
+					"datetime",
+					style = "%Y-%m-%d %I:%M %p",
+				} },
+				lualine_y = {},
+				lualine_z = {},
+			},
+			winbar = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = {},
+				lualine_x = {},
+				lualine_y = {},
+				lualine_z = {},
+			},
+
+			inactive_winbar = {
+				lualine_a = {},
+				lualine_b = {},
+				lualine_c = {},
+				lualine_x = {},
+				lualine_y = {},
+				lualine_z = {},
+			},
+		})
+	end,
 }
