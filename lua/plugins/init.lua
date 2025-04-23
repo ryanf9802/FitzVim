dc0eae4bdd4bd682afebb35bd533afc0f80bcff1
Ryan Fitzpatrick <ryanf9802@gmail.com>
2025-04-23 10:49:03 -0400

    added option for local config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,16 +1,39 @@
-return {
-  require("plugins.conform"),
-  require("plugins.moonfly"),
-  require("plugins.nvim-tree"),
-  require("plugins.telescope"),
-  require("plugins.treesitter"),
-  require("plugins.lsp"),
-  require("plugins.typescript-tools"),
-  require("plugins.cmp"),
-  require("plugins.autopairs"),
-  require("plugins.autotag"),
-  require("plugins.surround"),
-  require("plugins.supermaven"),
-  require("plugins.luasnip"),
-  require("plugins.golf"),
+-- Load local configuration override
+local ok, local_cfg = pcall(require, "local_config")
+local disabled = (ok and local_cfg.disable_plugins) or {}
+
+-- Plugin module names
+local modules = {
+  "plugins.conform",
+  "plugins.moonfly",
+  "plugins.nvim-tree",
+  "plugins.telescope",
+  "plugins.treesitter",
+  "plugins.lsp",
+  "plugins.typescript-tools",
+  "plugins.cmp",
+  "plugins.autopairs",
+  "plugins.autotag",
+  "plugins.surround",
+  "plugins.supermaven",
+  "plugins.luasnip",
+  "plugins.golf",
 }
+
+-- is this module in the disabled list?
+local function is_disabled(name)
+  for _, v in ipairs(disabled) do
+    if v == name then return true end
+  end
+  return false
+end
+
+-- require only the ones _not_ disabled
+local loaded = {}
+for _, name in ipairs(modules) do
+  if not is_disabled(name) then
+    table.insert(loaded, require(name))
+  end
+end
+
+return loaded
