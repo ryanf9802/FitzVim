a5ccdba38f92a1cf655bb51f4ab8311d9fa05b97
Ryan Fitzpatrick <ryanf9802@gmail.com>
2025-04-23 15:58:11 -0400

    changed theme back to catppuccin to fix lualine theming :(

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -2,10 +2,11 @@ return {
   "catppuccin/nvim",
   name = "catppuccin",
   priority = 1000,
+  enabled = true,
   config = function()
     require("catppuccin").setup({
       flavour = "mocha",
-      transparent_background = true
+      transparent_background = true,
     })
     vim.cmd.colorscheme("catppuccin")
   end
--- lua/plugins/moonfly.lua
+++ lua/plugins/moonfly.lua
@@ -2,6 +2,7 @@ return {
   "bluz71/vim-moonfly-colors",
   name = "moonfly",
   priority = 1000,
+  enabled = false,
   config = function()
     vim.g.moonflyTransparent = true
     vim.cmd([[colorscheme moonfly]])
