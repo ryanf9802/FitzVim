f297716ffe1e92495f9e7a906563146395b20ec1
Ryan Fitzpatrick <ryanf9802@gmail.com>
2025-07-28 22:35:52 -0400

    added fzf for snacks picker

---
+++ lua/plugins/fzf.lua
@@ -0,0 +1,21 @@
+return {
+	"ibhagwan/fzf-lua",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("fzf-lua").setup({
+			"default-title",
+			fzf_opts = {
+				["--layout"] = "reverse-list",
+			},
+			winopts = {
+				height = 0.85,
+				width = 0.80,
+				preview = {
+					layout = "flex",
+					flip_columns = 120,
+				},
+			},
+		})
+	end,
+	-- No keymaps - using snacks picker instead
+}
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -22,8 +22,21 @@ return {
 				enabled = true,
 			},
 			picker = {
+				enabled = true,
+				backend = "fzf-lua",
+				opts = {
+					fzf_opts = {
+						["--layout"] = "reverse-list",
+					},
+				},
 				sources = {
+					files = {
+						hidden = true,
+						no_ignore = false,
+					},
 					explorer = {
+						hidden = true,
+						no_ignore = false,
 						win = {
 							list = {
 								keys = {
@@ -44,6 +57,70 @@ return {
 				Snacks.explorer()
 			end,
 			desc = "File Explorer",
+		},
+		{
+			"<leader>ff",
+			function()
+				if vim.fn.system("git rev-parse --is-inside-work-tree 2>/dev/null"):match("true") then
+					Snacks.picker.git_files()
+				else
+					Snacks.picker.files()
+				end
+			end,
+			desc = "Find Files (Git-aware)",
+		},
+		{
+			"<leader>fg",
+			function()
+				if vim.fn.system("git rev-parse --is-inside-work-tree 2>/dev/null"):match("true") then
+					Snacks.picker.grep({ git = true })
+				else
+					Snacks.picker.grep()
+				end
+			end,
+			desc = "Live Grep (Git-aware)",
+		},
+		{
+			"<leader>fb",
+			function()
+				Snacks.picker.buffers()
+			end,
+			desc = "Find Buffers",
+		},
+		{
+			"<leader>fr",
+			function()
+				Snacks.picker.recent()
+			end,
+			desc = "Recent Files",
+		},
+		{
+			"<leader>fc",
+			function()
+				Snacks.picker.commands()
+			end,
+			desc = "Commands",
+		},
+		{
+			"<leader>fh",
+			function()
+				Snacks.picker.help()
+			end,
+			desc = "Help Tags",
+		},
+		{
+			"<leader>fF",
+			function()
+				Snacks.picker.files()
+			end,
+			desc = "Find Files (All files including gitignored)",
+		},
+		{
+			"<leader>fG",
+			function()
+				Snacks.picker.grep()
+			end,
+			desc = "Live Grep (All files including gitignored)",
 		},
 		{
 			"<leader>gb",
