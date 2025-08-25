e9db9d3cb31a336b67262972433f2646cb828939
Ryan Fitzpatrick <ryanf9802@gmail.com>
2025-08-25 14:45:39 -0400

    metals config

--- lua/plugins/metals.lua
+++ lua/plugins/metals.lua
@@ -1,22 +1,38 @@
 return {
-  "scalameta/nvim-metals",
-  ft = { "scala", "sbt", "java" },
-  opts = function()
-    local metals_config = require("metals").bare_config()
-    metals_config.on_attach = function(client, bufnr)
-      -- your on_attach function
-    end
+	"scalameta/nvim-metals",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	ft = { "scala", "sbt", "java" },
+	opts = function()
+		local metals_config = require("metals").bare_config()
 
-    return metals_config
-  end,
-  config = function(self, metals_config)
-    local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
-    vim.api.nvim_create_autocmd("FileType", {
-      pattern = self.ft,
-      callback = function()
-        require("metals").initialize_or_attach(metals_config)
-      end,
-      group = nvim_metals_group,
-    })
-  end
+		-- Use the same on_attach function as the other LSP servers
+		metals_config.on_attach = function(_, bufnr)
+			local opts = { buffer = bufnr, silent = true }
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+			vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+			vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, opts)
+		end
+
+		metals_config.settings = {
+			showImplicitArguments = true,
+			showInferredType = true,
+			superMethodLensesEnabled = true,
+		}
+
+		metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+		return metals_config
+	end,
+	config = function(self, metals_config)
+		local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = self.ft,
+			callback = function()
+				require("metals").initialize_or_attach(metals_config)
+			end,
+			group = nvim_metals_group,
+		})
+	end,
 }
