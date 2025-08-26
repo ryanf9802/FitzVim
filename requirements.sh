f865e1e2875d30d4f3aab46bb8bf9fe5a49381ed
Ryan Fitzpatrick <rfitzpatrick@npss-inc.com>
2025-08-26 11:10:55 -0400

    idempotency for pip pypi trusted-store config

--- requirements.sh
+++ requirements.sh
@@ -2,6 +2,24 @@
 
 sudo apt update
 
+# tell pip to trust the Python package servers
+PIP_CONF=~/.config/pip/pip.conf
+if ! grep -q "^\s*trusted-host" "$PIP_CONF" 2>/dev/null; then
+  echo "Pip Configuration not found. Adding it now..."
+  # Ensure the directory exists
+  mkdir -p "$(dirname "$PIP_CONF")"
+  # Append the configuration using a 'here document' for clarity
+  cat << EOF >> "$PIP_CONF"
+[global]
+trusted-host = pypi.org
+               files.pythonhosted.org
+               pypi.python.org
+EOF
+  echo "pip trusted-host configuration has been added to $PIP_CONF."
+else
+  echo "Pip Configuration already exists. No changes made."
+fi
+
 # install coursier for metals (Scala LSP)
 # for x86-64 (aka AMD64)
 sudo apt install openjdk-21-jdk # java 21 lts
