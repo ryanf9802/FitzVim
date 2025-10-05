ecb914f7b5cd6e58f05dacc224d6c7887ead957f
Ryan Fitzpatrick <ryanf9802@gmail.com>
2025-10-04 22:34:22 -0400

    fix upgrade

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -8,82 +8,90 @@ local specs = {
 		"neovim/nvim-lspconfig",
 		dependencies = { "b0o/schemastore.nvim" },
 		config = function()
-			local lspconfig = require("lspconfig")
 			local python_utils = require("utils.python")
+			local schemastore = require("schemastore")
 			local venv_python = python_utils.find_venv_python()
 
 			local on_attach = function(_, bufnr)
 				local opts = { buffer = bufnr, silent = true }
 				vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
 				vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+				vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+				vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 				vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, opts)
 			end
 
-			lspconfig.pyright.setup({
-				on_attach = on_attach,
-				settings = {
-					python = {
-						pythonPath = venv_python or "python",
-						disableVenvSearch = true,
+			local server_configs = {
+				pyright = {
+					settings = {
+						python = {
+							pythonPath = venv_python or "python",
+							disableVenvSearch = true,
+						},
 					},
 				},
-			})
-
-			lspconfig.lua_ls.setup({
-				on_attach = on_attach,
-				settings = {
-					Lua = {
-						diagnostics = {
-							globals = { "vim", "Snacks" },
-						},
-						workspace = {
-							library = {
-								vim.env.VIMRUNTIME,
-								"${3rd}/luv/library",
-								"${3rd}/busted/library",
+				lua_ls = {
+					settings = {
+						Lua = {
+							diagnostics = {
+								globals = { "vim", "Snacks" },
 							},
-							checkThirdParty = false,
-						},
-						telemetry = {
-							enable = false,
+							workspace = {
+								library = {
+									vim.env.VIMRUNTIME,
+									"${3rd}/luv/library",
+									"${3rd}/busted/library",
+								},
+								checkThirdParty = false,
+							},
+							telemetry = {
+								enable = false,
+							},
 						},
 					},
 				},
-			})
-
-			lspconfig.ts_ls.setup({
-				on_attach = on_attach,
-				filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
-			})
-
-			lspconfig.jsonls.setup({
-				on_attach = on_attach,
-				settings = {
-					json = {
-						schemas = require('schemastore').json.schemas(),
-						validate = { enable = true },
-					},
+				ts_ls = {
+					filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
 				},
-			})
-
-			lspconfig.bashls.setup({
-				on_attach = on_attach,
-			})
-
-			lspconfig.yamlls.setup({
-				on_attach = on_attach,
-				settings = {
-					yaml = {
-						schemaStore = {
-							enable = false,
-							url = "",
+				jsonls = {
+					settings = {
+						json = {
+							schemas = schemastore.json.schemas(),
+							validate = { enable = true },
 						},
-						schemas = require('schemastore').yaml.schemas(),
 					},
 				},
-			})
+				bashls = {},
+				yamlls = {
+					settings = {
+						yaml = {
+							schemaStore = {
+								enable = false,
+								url = "",
+							},
+							schemas = schemastore.yaml.schemas(),
+						},
+					},
+				},
+			}
+
+			if vim.lsp and vim.lsp.enable then
+				vim.lsp.config("*", { on_attach = on_attach })
+				for name, cfg in pairs(server_configs) do
+					if cfg and next(cfg) ~= nil then
+						vim.lsp.config(name, cfg)
+					end
+				end
+				vim.lsp.enable(vim.tbl_keys(server_configs))
+			else
+				local lspconfig = require("lspconfig")
+				for name, cfg in pairs(server_configs) do
+					local server = lspconfig[name]
+					if server and server.setup then
+						server.setup(vim.tbl_deep_extend("force", { on_attach = on_attach }, cfg or {}))
+					end
+				end
+			end
 
 			vim.diagnostic.config({
 				virtual_text = true,
@@ -93,8 +101,7 @@ local specs = {
 				severity_sort = true,
 			})
 		end,
-}
-
+	},
 }
 
 if not cli_diagnostics_mode then
@@ -133,7 +140,7 @@ if not cli_diagnostics_mode then
 				},
 				run_on_start = true,
 				auto_update = true,
-		})
+			})
 		end,
 	})
 end
